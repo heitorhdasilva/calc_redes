f9593702d9990f7a76642a52e1be1ae4210f8b8f
Alice Borges <aliceborges@outlook.com.br>
2018-06-29 16:58:31 -0300

    ola

--- exe.php
+++ exe.php
@@ -1,11 +1,11 @@
 <?php
 
-$rede = 24; //$_GET['rede'];
-$mask = 25; //$_GET['mask'];
+$rede = $_GET['rede'];
+$mask = $_GET['mask'];
 
         $quantredes = pow(2,$mask-24);
         $host = 256/$quantredes;
-        $rede = ceil($rede/$host) ;
+        $rede = floor($rede/$host) ;
         $iprede = $rede*$host;
         $broadcast = ($iprede+$host)-1;
         $privalido = $iprede+1;
@@ -20,7 +20,6 @@ $data = array(
     'primeiro_valido'=>$privalido,
     'ultimo_valido'=>$ultvalido);
 
-//header('Content-type: application/json');
-//echo json_encode($data);
+header('Content-type: application/json');
+echo json_encode($data);
 
-echo $rede;
