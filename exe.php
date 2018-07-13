7cb6a3e8a9d1d2899197359a0b4107940ddbc7dd
heitorhdasilva <orapideks@gmail.com>
2018-07-13 09:33:06 -0300

    ola

--- exe.php
+++ exe.php
@@ -2,6 +2,8 @@
 
 $rede = $_GET['rede'];
 $mask = $_GET['mask'];
+$prim = $_GET['prim'];
+$segun = $_GET['segun'];
 
         $quantredes = pow(2,$mask-24);
         $host = 256/$quantredes;
@@ -10,6 +12,27 @@ $mask = $_GET['mask'];
         $broadcast = ($iprede+$host)-1;
         $privalido = $iprede+1;
         $ultvalido = $broadcast-1;
+        $maskdec = 256-$host;
+        if ($prim >= 224 and $prim <= 239){
+            $classe = 'D';
+        }elseif ($prim <=  223 and $prim >= 192){
+            $classe = 'C';
+        }elseif ($prim <= 191 and $prim >= 128){
+            $classe = 'B';
+        }elseif ($prim <= 126 and $prim >=  1){
+            $classe = 'A';
+        }else{
+            $classe = 'E';
+        };
+        if ($prim == 10 and $mask == 8){
+            $ipval = 'privado';
+        }elseif ($prim == 172 and $mask == 12 and $segun >= 16 and $segun <=31){
+                $ipval = 'privado';
+        }elseif ($prim == 192 and $segun == 168){
+            $ipval = 'privado';
+        }else{
+            $ipval = 'publico';
+        };
 
 $data = array(
     'host'=>$host,
@@ -18,7 +41,10 @@ $data = array(
     'iprede'=>$iprede,
     'broadcast'=>$broadcast,
     'primeiro_valido'=>$privalido,
-    'ultimo_valido'=>$ultvalido);
+    'ultimo_valido'=>$ultvalido,
+    'maskdec'=>$maskdec,
+    'classe'=>$classe,
+    'ipval'=>$ipval);
 
 header('Content-type: application/json');
 echo json_encode($data);
